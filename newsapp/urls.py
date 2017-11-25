f388ff45fbc3f72a27fdb2b165c014a37d739685
thanga <aksthanga@gmail.com>
2017-11-25 22:25:32 +0530

    rss link

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from newsapp import views
+#sfrom newsapp import feed
 
 urlpatterns = [
 	path ('', views.index),
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.syndication.views import Feed
 
 # Create your views here.
 def index(request):
-	return render(request,"base.html")
+	#passing lis of docts to templates 
+	news = [
+	{'title': 'item_title', 'description': 'This is the test2 description'},
+	{'title': 'News 2', 'description': 'This is the test2 description'}
+	]
+	return render(request,'base.html',{'news': news} )
+
+
+
+
