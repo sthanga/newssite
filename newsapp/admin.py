0982734aa0ec8661e3a4e3a11780df2a9bcf31a1
thanga <aksthanga@gmail.com>
2017-11-29 00:37:01 +0530

    rss finished

--- newsapp/admin.py
+++ newsapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from newsapp.models import NewsLink
 
 # Register your models here.
+admin.site.register(NewsLink)
--- newsapp/views.py
+++ newsapp/views.py
@@ -2,12 +2,13 @@ import feedparser
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.syndication.views import Feed
+from newsapp.models import NewsLink
 
 # Create your views here.
 def index(request):
 	#passing lis of docts to templates 
-
-	feed = feedparser.parse('http://zeenews.india.com/tamil/india.xml')
+	link = NewsLink.objects.get(id=1)
+	feed = feedparser.parse(link.rss) #'http://zeenews.india.com/tamil/india.xml'
 
 	return render(request,'base.html',{'feed': feed} )
 
